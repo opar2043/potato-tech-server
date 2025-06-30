021ec582a4908b610162be561065b406a85027f4
opar2043 <rezonerashidopar@gmail.com>
2025-07-01 00:29:00 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,131 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const PORT = process.env.PORT || 5001;
+
+const app = express();
+app.use(express.json()); // Parse JSON requests
+app.use(cors()); 
+
+
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+// const uri =
+//   "mongodb+srv://task-manager:8xV6VIRQIxbvQslA@cluster0.xfvkq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = "mongodb+srv://potatoTech:CbGpBaja49fj3lC6@cluster0.xfvkq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+
+    const db = client.db("potatoTech");
+    const productCollection = db.collection("products");
+    const reviewCollection = db.collection("reviews");
+    const userCollection = db.collection("users");
+
+    app.post("/products", async (req, res) => {
+      const products = req.body;
+      const result = await productCollection.insertOne(products);
+      res.send(result);
+    });
+
+
+    app.post('/reviews', async(req,res)=>{
+      const review = req.body;
+      const result = await reviewCollection.insertOne(review);
+      res.send(result)
+    })
+
+    app.get("/reviews", async (req, res) => {
+      const result = await reviewCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.get("/products", async (req, res) => {
+      const result = await productCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.delete('/products/:id', async(req, res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await productCollection.deleteOne(query);
+      res.send(result)
+    })
+
+    app.post('/users' , async(req,res)=>{
+        const users = req.body;
+        const result = await userCollection.insertOne(users);
+        res.send(result)
+    })
+    
+    app.get('/users' , async(req, res) => {
+        const result = await userCollection.find().toArray();
+        res.send(result)
+    })
+
+
+    // Send a ping to confirm a successful connection
+    // await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+// API Routes
+app.get("/", (req, res) => {
+  res.send("Potato Tech server Running");
+});
+
+// Start Server
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+
+
+
+
+
+
+
+
+
+
+
+// const { MongoClient, ServerApiVersion } = require('mongodb');
+
+// // Create a MongoClient with a MongoClientOptions object to set the Stable API version
+// const client = new MongoClient(uri, {
+//   serverApi: {
+//     version: ServerApiVersion.v1,
+//     strict: true,
+//     deprecationErrors: true,
+//   }
+// });
+
+// async function run() {
+//   try {
+//     // Connect the client to the server	(optional starting in v4.7)
+//     await client.connect();
+//     // Send a ping to confirm a successful connection
+//     await client.db("admin").command({ ping: 1 });
+//     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+//   } finally {
+//     // Ensures that the client will close when you finish/error
+//     await client.close();
+//   }
+// }
+// run().catch(console.dir);
