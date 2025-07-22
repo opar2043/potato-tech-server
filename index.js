cc5dad0c232470723f86505426560153809f3829
opar2043 <rezonerashidopar@gmail.com>
2025-07-22 18:52:57 +0600

    user role added

--- index.js
+++ index.js
@@ -147,6 +147,21 @@ async function run() {
       const result = await userCollection.updateOne(filter, updateDoc);
       res.send(result);
     });
+    
+    app.patch("/users/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+
+      const updateDoc = {
+        $set: {
+          role: "customer",
+        },
+      };
+
+      const result = await userCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    });
+
 
     // Orders  ==============================================
 
